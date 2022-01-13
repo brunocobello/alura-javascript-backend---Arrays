4064bb69731d2d5a62455a00e1eb5e9211bbca99
Bruno <bruno-alura-git@gmail.com>
2022-01-13 15:22:48 -0300

    aula_01 - introducao a Arrays

---
+++ aula_01/01_array_simples.js
@@ -0,0 +1,8 @@
+// construindo um array de números
+const listaN = [10, 20, 45, 60];
+
+// calculando a média
+let mediaFinal = (listaN[0] + listaN[1] + listaN[2] + listaN[3]) / listaN.length;
+
+// exibindo o resultado no console
+console.log(`A media final é ${mediaFinal}`);
